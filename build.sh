e6522ad761196d79108f927f409b75f8ab490825
Jason Stafford <jstafford@thetus.com>
2015-08-26 11:12:58 -0700

    Stylistic change of if then constructs

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ EX_USAGE=64
 #		system files.
 EX_DATAERR=65
 
-if [ $# -ne 2 ]
-then
+if [ $# -ne 2 ]; then
   echo "Exactly two arguments are required. For example './build.sh 5.3.0 1' where '5.3.0' can be replaced by any released version of Solr and the second parameter is the version of the rpm itself"
   exit $EX_USAGE
 fi
@@ -20,16 +19,14 @@ SOLR_VERSION="$1"
 RPM_RELEASE="$2"
 
 # validate the inputs
-if [[ "$SOLR_VERSION" =~ ^5\.[0-9]+\.[0-9]+$ ]]
-then
+if [[ "$SOLR_VERSION" =~ ^5\.[0-9]+\.[0-9]+$ ]]; then
   echo "Using Solr version: $SOLR_VERSION"
 else
   echo "Invalid Solr version format.  Must be of the form 5.x.x"
   exit $EX_USAGE
 fi
 
-if [[ "$RPM_RELEASE" =~ ^[0-9]+$ ]]
-then
+if [[ "$RPM_RELEASE" =~ ^[0-9]+$ ]]; then
   echo "Building rpm version: $RPM_RELEASE"
 else
   echo "Invalid release version format.  Must be a single number"
@@ -41,28 +38,25 @@ rm -rf BUILD RPMS SRPMS tmp || true
 mkdir -p BUILD RPMS SRPMS tmp
 
 # download the sources
-if [ ! -d SOURCES ]
-then
+if [ ! -d SOURCES ]; then
   mkdir SOURCES
 fi
 
-if [ ! -f SOURCES/solr-$SOLR_VERSION.tgz ]
-then
+if [ ! -f SOURCES/solr-$SOLR_VERSION.tgz ]; then
   # download the list of mirror sites
   wget -O tmp/mirrors.html http://www.apache.org/dyn/closer.cgi/lucene/solr/$SOLR_VERSION
   # use hxwls from the w3.org html-xml-utils package to get the links out of the html file
   ALL_LINKS=$(hxwls tmp/mirrors.html)
   # grab the fist link that contains the version we are looking for
   DOWNLOAD_LINK=$(grep -m 1 "$SOLR_VERSION" <<< "$ALL_LINKS")
-  # actually download the archive from the mirror	
+  # actually download the archive from the mirror
   wget -O SOURCES/solr-$SOLR_VERSION.tgz $DOWNLOAD_LINK/solr-$SOLR_VERSION.tgz
 fi
 
 # get the SHA1 from Apache directly
-if [ ! -f SOURCES/solr-$SOLR_VERSION.tgz.sha1 ]
-then
+if [ ! -f SOURCES/solr-$SOLR_VERSION.tgz.sha1 ]; then
   wget -O SOURCES/solr-$SOLR_VERSION.tgz.sha1 http://archive.apache.org/dist/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz.sha1
-fi 
+fi
 
 # verify the integrity of the archive
 # the sha1 file has the file name at the end, so use a regex to just pull the sha1 part out of the file
@@ -76,13 +70,9 @@ LOCAL_SHA1=$(openssl sha1 SOURCES/solr-$SOLR_VERSION.tgz)
 [[ $LOCAL_SHA1 =~ ([0-9a-f]+)$ ]]
 LOCAL_SHA1="${BASH_REMATCH[1]}"
 
-if [ $LOCAL_SHA1 == $SHA1 ]
-then
+if [ $LOCAL_SHA1 == $SHA1 ]; then
   echo "SHA1 for SOURCES/solr-$SOLR_VERSION.tgz checks out"
 else
   echo "ERROR! SOURCES/solr-$SOLR_VERSION.tgz has become corrupted or been tampered with.  Delete it and re-run this script."
   exit $EX_DATAERR
 fi
-
-
-
